023fd1e0ff9fa14c312796e2e8214c2a5ada142b
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 07:52:32 -0600

    Add RenameProviderColumnInProductToBuy migration

---
+++ Mux/Migrations/20220221135036_RenameProviderColumnInProductToBuy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class RenameProviderColumnInProductToBuy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Provider",
+                table: "ShoppingCart",
+                newName: "OldProvider");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OldProvider",
+                table: "ShoppingCart",
+                newName: "Provider");
+        }
+    }
+}
