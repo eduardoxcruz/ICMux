acf02380ae6160501cbb89ab59112070383d86a9
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 12:09:22 -0600

    Add properties

--- Mux/Model/Manufacturer.cs
+++ Mux/Model/Manufacturer.cs
@@ -1,6 +1,10 @@
-﻿namespace Mux.Model;
+﻿using System.Collections.Generic;
+
+namespace Mux.Model;
 
 public class Manufacturer
 {
-	
+	public int Id { get; set; }
+	public string BusinessName { get; set; }
+	public List<Product> Products { get; set; }
 }
