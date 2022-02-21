d87e0e6a184898b615abdd9aa6b026de3e020153
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:07:27 -0600

    Add ProductCategoriesRelationship class

---
+++ Mux/Relationship/ProductCategoriesRelationship.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationship
+{
+	internal class ProductCategoriesRelationship : RelationshipConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>()
+				.HasMany(product => product.Categories)
+				.WithMany(category => category.Products)
+				.UsingEntity<Dictionary<string, object>>(
+					"ProductCategories",
+					entityTypeBuilder => entityTypeBuilder
+						.HasOne<Category>()
+						.WithMany()
+						.HasForeignKey("CategoryId")
+						.HasConstraintName("FK_ProductCategories_Categories_CategoryId")
+						.OnDelete(DeleteBehavior.ClientSetNull),
+					entityTypeBuilder => entityTypeBuilder
+						.HasOne<Product>()
+						.WithMany()
+						.HasForeignKey("ProductId")
+						.HasConstraintName("FK_ProductCategories_Products_ProductId")
+						.OnDelete(DeleteBehavior.ClientSetNull)
+				);
+		}
+	}
+}
