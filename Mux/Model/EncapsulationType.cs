a68940a0a1c0624595b3c03e5b6a45c799d2503b
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 02:41:48 -0600

    Add EncapsulationType class

---
+++ Mux/Model/EncapsulationType.cs
@@ -0,0 +1,19 @@
+﻿namespace Mux.Model
+{
+	public class EncapsulationType
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string BodyWidth { get; set; }
+		public string FullDescription { get; set; }
+		public List<Product> Products { get; set; }
+
+		public EncapsulationType()
+		{
+			Id = 0;
+			Name = "";
+			BodyWidth = "";
+			FullDescription = "";
+		}
+	}
+}
