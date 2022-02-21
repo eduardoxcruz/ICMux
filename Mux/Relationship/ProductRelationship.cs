31ded56173543df55b8e46c0f1f60516ed4ad9e5
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:07:56 -0600

    Add ProductRelationship class

---
+++ Mux/Relationship/ProductRelationship.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationship
+{
+	internal class ProductRelationship : RelationshipConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Product>()
+				.HasOne(product => product.EncapsulationType)
+				.WithMany(encapsulationType => encapsulationType.Products)
+				.HasForeignKey(product => product.EncapsulationTypeId)
+				.OnDelete(DeleteBehavior.ClientCascade);
+		}
+	}
+}
