94b412afc0d3ffd03ae39a7f61c6b19819a3adcf
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 02:44:56 -0600

    Add ProductChangeLog class

---
+++ Mux/Model/ProductChangeLog.cs
@@ -0,0 +1,36 @@
+﻿namespace Mux.Model
+{
+	public class ProductChangeLog
+	{
+#pragma warning disable 8632
+		public int? Id { get; set; }
+		public DateTime? Date { get; set; }
+		public string? Type { get; set; }
+		public int? Amount { get; set; }
+		public int? PreviousAmount { get; set; }
+		public int? NewAmount { get; set; }
+		public decimal? PurchasePrice { get; set; }
+		public string? Provider { get; set; }
+		public string? ProductFullDescription { get; set; }
+		public string? EmployeeName { get; set; }
+		public Employee Employee { get; set; }
+		public int? EmployeeId { get; set; }
+		public Product Product { get; set; }
+		public int? ProductId { get; set; }
+#pragma warning restore 8632
+		
+		public ProductChangeLog()
+		{
+			Id = 0;
+			Date = DateTime.Now;
+			Type = "";
+			Amount = 0;
+			PreviousAmount = 0;
+			NewAmount = 0;
+			PurchasePrice = 0;
+			Provider = "";
+			ProductFullDescription = "";
+			EmployeeName = "";
+		}
+	}
+}
