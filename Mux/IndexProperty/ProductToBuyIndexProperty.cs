57c14e6f42c38639ede724990a440cd31d811527
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:05:46 -0600

    Add ProductToBuyIndexProperty class

---
+++ Mux/IndexProperty/ProductToBuyIndexProperty.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.IndexProperty
+{
+	internal class ProductToBuyIndexProperty : IndexPropertyConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasIndex(productForBuy => productForBuy.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasIndex(productForBuy => productForBuy.EmployeeId)
+				.IsUnique(false);
+		}
+	}
+}
