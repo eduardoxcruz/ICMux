67b50ecb01a31643633b60ca4aa6511ce816f588
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 02:41:29 -0600

    Add Employee class

---
+++ Mux/Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Mux.Model
+{
+	public class Employee
+	{
+#pragma warning disable 8632
+		public int Id { get; set; }
+		public string? Type { get; set; }
+		public string FullName { get; set; }
+		public string? Password { get; set; }
+		public List<ProductToBuy> ShoppingCart { get; set; }
+		public List<ProductRequest> ProductRequests { get; set; }
+		public List<ProductChangeLog> ProductChangeLogs { get; set; }
+#pragma warning restore 8632
+		
+		public Employee()
+		{
+			Id = 0;
+			Type = "";
+			FullName = "";
+			Password = "";
+		}
+	}
+}
