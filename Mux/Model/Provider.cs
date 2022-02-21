7627388ce1154f1cbcb3755b0eef6c3ee6217f7d
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 07:29:12 -0600

    Add ProductChangelogs property

--- Mux/Model/Provider.cs
+++ Mux/Model/Provider.cs
@@ -1,9 +1,12 @@
-﻿namespace Mux.Model;
+﻿using System.Collections.Generic;
+
+namespace Mux.Model;
 
 public class Provider
 {
 	public int Id { get; set; }
 	public string BusinessName { get; set; }
+	public List<ProductChangelog> ProductChangelogs { get; set; }
 
 	public Provider()
 	{
