a76c75a1e8c61bc120c74e0e790c3be62e7ec12e
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:08:23 -0600

    Add ShoppingCartRelationship class

---
+++ Mux/Relationship/ShoppingCartRelationship.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationship
+{
+	internal class ShoppingCartRelationship : RelationshipConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasOne(productForBuy => productForBuy.Product)
+				.WithMany(product => product.ShoppingCart)
+				.HasForeignKey(productForBuy => productForBuy.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<ProductToBuy>()
+				.HasOne(productForBuy => productForBuy.Employee)
+				.WithMany(employee => employee.ShoppingCart)
+				.HasForeignKey(productForBuy => productForBuy.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
+	}
+}
