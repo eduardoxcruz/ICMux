4fa46e223bb57815fecd5e9e7f0d85d252d5bfb0
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 13:29:58 -0600

    Add IEntityTypeConfiguration implementation

--- Mux/Entity/UnitTypeEntity.cs
+++ Mux/Entity/UnitTypeEntity.cs
@@ -1,6 +1,17 @@
-﻿namespace Mux.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
 
-internal class UnitTypeEntity
+namespace Mux.Entity;
+
+internal class UnitTypeEntity : IEntityTypeConfiguration<UnitType>
 {
-	
+	public void Configure(EntityTypeBuilder<UnitType> builder)
+	{
+		builder.HasIndex(unitType => unitType.Id);
+
+		builder.Property(unitType => unitType.Id).ValueGeneratedOnAdd();
+
+		builder.Property(unitType => unitType.Name).HasMaxLength(30).IsUnicode();
+	}
 }
