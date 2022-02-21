15feed1a1c0e9919da1decaa4c3e5f657824712d
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 13:34:08 -0600

    Add NavigationPropertyConfiguration implement

--- Mux/NavigationProperty/UnitTypeNavigationProperty.cs
+++ Mux/NavigationProperty/UnitTypeNavigationProperty.cs
@@ -1,6 +1,14 @@
-﻿namespace Mux.NavigationProperty;
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
 
-internal class UnitTypeNavigationProperty
+namespace Mux.NavigationProperty;
+
+internal class UnitTypeNavigationProperty : NavigationPropertyConfiguration
 {
-	
+	public void Configure(ref ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<UnitType>()
+			.Navigation(unitType => unitType.Products)
+			.UsePropertyAccessMode(PropertyAccessMode.Property);
+	}
 }
