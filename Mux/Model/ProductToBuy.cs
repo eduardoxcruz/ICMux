16be3f5d3f5a214250a7889dde759b7e358d3b42
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 02:46:24 -0600

    Add ProductToBuy class

---
+++ Mux/Model/ProductToBuy.cs
@@ -0,0 +1,28 @@
+﻿namespace Mux.Model
+{
+	public class ProductToBuy
+	{
+#pragma warning disable 8632
+		public int? Id { get; set; }
+		public string? Provider { get; set; }
+		public DateTime? Date { get; set; }
+		public string? Status { get; set; }
+		public int? RequestedAmount { get; set; }
+		public string? EmployeeName { get; set; }
+		public Employee Employee { get; set; }
+		public int? EmployeeId { get; set; }
+		public Product Product { get; set; }
+		public int? ProductId { get; set; }
+#pragma warning restore 8632
+		
+		public ProductToBuy()
+		{
+			Id = 0;
+			Provider = "";
+			Date = DateTime.Now;
+			Status = "";
+			RequestedAmount = 0;
+			EmployeeName = "";
+		}
+	}
+}
