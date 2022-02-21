dcec03ebb76deaf4d1fdf1bf4aed3baf7e0b651f
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 12:31:49 -0600

    Add IEntityTypeConfiguration implementation

--- Mux/Entity/ManufacturerEntity.cs
+++ Mux/Entity/ManufacturerEntity.cs
@@ -1,6 +1,17 @@
-﻿namespace Mux.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
 
-internal class ManufacturerEntity
+namespace Mux.Entity;
+
+internal class ManufacturerEntity : IEntityTypeConfiguration<Manufacturer>
 {
-	
+	public void Configure(EntityTypeBuilder<Manufacturer> builder)
+	{
+		builder.HasIndex(manufacturer => manufacturer.Id);
+
+		builder.Property(m => m.Id).ValueGeneratedOnAdd();
+
+		builder.Property(m => m.BusinessName).HasMaxLength(50).IsUnicode();
+	}
 }
