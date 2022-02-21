dab7aadba6a41558c0aa07f4cba1246692daebbd
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:08:38 -0600

    Add ICContext class

---
+++ Mux/ICContext.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Entity;
+using Mux.IndexProperty;
+using Mux.Model;
+using Mux.NavigationProperty;
+using Mux.Relationship;
+
+namespace Mux;
+
+public class ICContext : DbContext
+{
+	private readonly string _connectionString;
+
+	public virtual DbSet<ProductToBuy> ShoppingCart { get; set; }
+	public virtual DbSet<ProductChangelog> ProductChangelogs { get; set; }
+	public virtual DbSet<Product> Products { get; set; }
+	public virtual DbSet<ProductRequest> ProductRequests { get; set; }
+	public virtual DbSet<Employee> Employees { get; set; }
+	public virtual DbSet<Category> Categories { get; set; }
+	public virtual DbSet<EncapsulationType> EncapsulationTypes { get; set; }
+
+	public ICContext()
+	{
+		_connectionString = "Server=192.168.0.254;Database=Testing;User Id=sa;Password=Tlacua015;";
+	}
+
+	public ICContext(string connectionString)
+	{
+		_connectionString = connectionString;
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(_connectionString);
+		optionsBuilder.EnableDetailedErrors();
+		optionsBuilder.EnableSensitiveDataLogging();
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+		StartEntityTypesConfiguration(ref modelBuilder);
+		StartRelationshipsConfiguration(ref modelBuilder);
+		StartNavigationPropertiesConfiguration(ref modelBuilder);
+		StartIndexPropertiesConfiguration(ref modelBuilder);
+	}
+
+	private static void StartEntityTypesConfiguration(ref ModelBuilder modelBuilder)
+	{
+		new ProductToBuyEntity().Configure(modelBuilder.Entity<ProductToBuy>());
+		new ProductChangelogEntity().Configure(modelBuilder.Entity<ProductChangelog>());
+		new ProductEntity().Configure(modelBuilder.Entity<Product>());
+		new ProductRequestEntity().Configure(modelBuilder.Entity<ProductRequest>());
+		new EmployeeEntity().Configure(modelBuilder.Entity<Employee>());
+		new CategoryEntity().Configure(modelBuilder.Entity<Category>());
+		new EncapsulationTypeEntity().Configure(modelBuilder.Entity<EncapsulationType>());
+	}
+
+	private static void StartRelationshipsConfiguration(ref ModelBuilder modelBuilder)
+	{
+		new ProductCategoriesRelationship().Configure(ref modelBuilder);
+		new ProductChangelogRelationship().Configure(ref modelBuilder);
+		new ProductRequestRelationship().Configure(ref modelBuilder);
+		new ShoppingCartRelationship().Configure(ref modelBuilder);
+		new ProductRelationship().Configure(ref modelBuilder);
+	}
+
+	private static void StartNavigationPropertiesConfiguration(ref ModelBuilder modelBuilder)
+	{
+		new ProductNavigationProperty().Configure(ref modelBuilder);
+		new EmployeeNavigationProperty().Configure(ref modelBuilder);
+		new EncapsulationTypeNavigationProperty().Configure(ref modelBuilder);
+	}
+
+	private static void StartIndexPropertiesConfiguration(ref ModelBuilder modelBuilder)
+	{
+		new ProductChangelogIndexProperty().Configure(ref modelBuilder);
+		new ProductRequestIndexProperty().Configure(ref modelBuilder);
+		new ProductToBuyIndexProperty().Configure(ref modelBuilder);
+		new ProductIndexProperty().Configure(ref modelBuilder);
+	}
+}
