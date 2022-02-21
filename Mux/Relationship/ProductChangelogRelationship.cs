ea7b07dd57dcb5c2cfe4f7dd084c13eba95f9a50
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:07:42 -0600

    Add ProductChangelogRelationship class

---
+++ Mux/Relationship/ProductChangelogRelationship.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.Relationship
+{
+	internal class ProductChangelogRelationship : RelationshipConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductChangelog>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Product)
+				.WithMany(product => product.ProductChangeLogs)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.ProductId)
+				.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder
+				.Entity<ProductChangelog>()
+				.HasOne(recordOfProductMovement => recordOfProductMovement.Employee)
+				.WithMany(employee => employee.ProductChangelogs)
+				.HasForeignKey(recordOfProductMovement => recordOfProductMovement.EmployeeId)
+				.OnDelete(DeleteBehavior.SetNull);
+		}
+	}
+}
