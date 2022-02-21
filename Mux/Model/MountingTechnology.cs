1af55b095a447fb9d002a089866380268a2a9095
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 07:03:07 -0600

    Add properties

--- Mux/Model/MountingTechnology.cs
+++ Mux/Model/MountingTechnology.cs
@@ -1,5 +1,10 @@
-﻿namespace Mux.Model;
+﻿using System.Collections.Generic;
+
+namespace Mux.Model;
 
 public class MountingTechnology
 {
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public List<Product> Products { get; set; }
 }
