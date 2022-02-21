964506f6ad7b4e1b7275364370b6b2908530b9c6
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 07:32:08 -0600

    Add RenameProviderColumn migration

---
+++ Mux/Migrations/20220221133059_RenameProviderColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class RenameProviderColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Provider",
+                table: "ProductChangeLogs",
+                newName: "OldProvider");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OldProvider",
+                table: "ProductChangeLogs",
+                newName: "Provider");
+        }
+    }
+}
