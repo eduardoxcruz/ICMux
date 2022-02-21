8bcf1b363366a5fb6a51f2862ba01217ad5f9c9a
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 12:27:19 -0600

    Add RenameManufacturerColumnInProduct migration

---
+++ Mux/Migrations/20220221181354_RenameManufacturerColumnInProduct.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class RenameManufacturerColumnInProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Manufacturer",
+                table: "Products",
+                newName: "OldManufacturer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OldManufacturer",
+                table: "Products",
+                newName: "Manufacturer");
+        }
+    }
+}
