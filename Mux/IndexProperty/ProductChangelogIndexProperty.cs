c3b97c40e0f2c799bd89800804dcec175f7f53ca
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:05:02 -0600

    Add ProductChangelogIndexProperty class

---
+++ Mux/IndexProperty/ProductChangelogIndexProperty.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mux.Model;
+
+namespace Mux.IndexProperty
+{
+	internal class ProductChangelogIndexProperty : IndexPropertyConfiguration
+	{
+		public void Configure(ref ModelBuilder modelBuilder)
+		{
+			modelBuilder
+				.Entity<ProductChangelog>()
+				.HasIndex(recordOfProductMovement => recordOfProductMovement.ProductId)
+				.IsUnique(false);
+
+			modelBuilder
+				.Entity<ProductChangelog>()
+				.HasIndex(recordOfProductMovement => recordOfProductMovement.EmployeeId)
+				.IsUnique(false);
+		}
+	}
+}
