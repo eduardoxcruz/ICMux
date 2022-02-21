2ed6ba55710218eb162fe0307273a4a8e7f83dca
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 11:49:48 -0600

    Add IEntityTypeConfiguration implementation

--- Mux/Entity/ProviderEntity.cs
+++ Mux/Entity/ProviderEntity.cs
@@ -1,6 +1,17 @@
-﻿namespace Mux.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mux.Model;
 
-public class ProviderEntity
+namespace Mux.Entity;
+
+public class ProviderEntity : IEntityTypeConfiguration<Provider>
 {
-	
+	public void Configure(EntityTypeBuilder<Provider> builder)
+	{
+		builder.HasIndex(provider => provider.Id);
+
+		builder.Property(provider => provider.Id).ValueGeneratedOnAdd();
+
+		builder.Property(provider => provider.BusinessName).HasMaxLength(50).IsUnicode();
+	}
 }
