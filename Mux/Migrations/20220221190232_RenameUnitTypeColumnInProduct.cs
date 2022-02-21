9b752a70f1c29c39dd114449dc756d58a8a65e9f
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 13:06:11 -0600

    Add RenameUnitTypeColumnInProduct migration

---
+++ Mux/Migrations/20220221190232_RenameUnitTypeColumnInProduct.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class RenameUnitTypeColumnInProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UnitType",
+                table: "Products",
+                newName: "OldUnitType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OldUnitType",
+                table: "Products",
+                newName: "UnitType");
+        }
+    }
+}
