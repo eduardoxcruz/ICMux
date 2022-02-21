5e441f1e44220dedacb39789c1e406caac8f3512
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 07:03:32 -0600

    Add default constructor

--- Mux/Model/MountingTechnology.cs
+++ Mux/Model/MountingTechnology.cs
@@ -7,4 +7,10 @@ public class MountingTechnology
 	public int Id { get; set; }
 	public string Name { get; set; }
 	public List<Product> Products { get; set; }
+
+	public MountingTechnology()
+	{
+		Id = 0;
+		Name = "";
+	}
 }
