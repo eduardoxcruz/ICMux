981c858bd4adb8ff78f4535bb73ed868a4049b26
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 02:45:30 -0600

    Add ProductRequest class

---
+++ Mux/Model/ProductRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace Mux.Model
+{
+	public class ProductRequest
+	{
+#pragma warning disable 8632
+		public int? Id { get; set; }
+		public DateTime? Date { get; set; }
+		public int? Amount { get; set; }
+		public string? EmployeeName { get; set; }
+		public string? Type { get; set; }
+		public string? Status { get; set; }
+		public Employee Employee { get; set; }
+		public int? EmployeeId { get; set; }
+		public Product Product { get; set; }
+		public int? ProductId { get; set; }
+#pragma warning restore 8632
+		
+		public ProductRequest()
+		{
+			Id = 0;
+			Date = DateTime.Now;
+			Amount = 0;
+			EmployeeName = null;
+			Type = "";
+			Status = "";
+		}
+	}
+}
