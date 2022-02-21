77921e8c85ca6f468db4c5cd5f18b956be9f9c7f
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-21 03:06:09 -0600

    Add NavigationPropertyConfiguration interface

---
+++ Mux/NavigationProperty/NavigationPropertyConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mux.NavigationProperty
+{
+	internal interface NavigationPropertyConfiguration
+	{
+		void Configure(ref ModelBuilder modelBuilder);
+	}
+}
