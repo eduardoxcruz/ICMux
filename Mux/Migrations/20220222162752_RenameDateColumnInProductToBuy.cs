4c1881a86a36e7eebcbea3850d5f9303b46f70f6
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 10:30:33 -0600

    Add RenameDateColumnInProductToBuy migration

---
+++ Mux/Migrations/20220222162752_RenameDateColumnInProductToBuy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class RenameDateColumnInProductToBuy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "ShoppingCart",
+                newName: "RequestDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RequestDate",
+                table: "ShoppingCart",
+                newName: "Date");
+        }
+    }
+}
