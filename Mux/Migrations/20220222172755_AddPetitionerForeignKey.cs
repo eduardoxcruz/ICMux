b1f011015b434afa17bd6b10487e0f2a6f041588
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 11:31:06 -0600

    Add AddPetitionerForeignKey

---
+++ Mux/Migrations/20220222172755_AddPetitionerForeignKey.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class AddPetitionerForeignKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PetitionerId",
+                table: "ShoppingCart",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCart_PetitionerId",
+                table: "ShoppingCart",
+                column: "PetitionerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCart_Employees_PetitionerId",
+                table: "ShoppingCart",
+                column: "PetitionerId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCart_Employees_PetitionerId",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ShoppingCart_PetitionerId",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropColumn(
+                name: "PetitionerId",
+                table: "ShoppingCart");
+        }
+    }
+}
