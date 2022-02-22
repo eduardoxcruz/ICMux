e975c8bc5755089c714f194fea9e1fc082d30904
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 13:48:03 -0600

    Refactorize class

--- Mux/Model/Product.cs
+++ Mux/Model/Product.cs
@@ -7,34 +7,34 @@ namespace Mux.Model
 #pragma warning disable 8632
 		public int Id { get; set; }
 		public string? DebugCode { get; set; }
-		public string? Status { get; set; }
-		public string? Enrollment { get; set; }
-		public string? OldMountingTechnology { get; set; }
-		public string? OldEncapsulationType { get; set; }
-		public string? ShortDescription { get; set; }
-		public string? Category { get; set; }
-		public bool? IsUsingInventory { get; set; }
-		public int? CurrentAmount { get; set; }
-		public int? MinAmount { get; set; }
-		public int? MaxAmount { get; set; }
+		public string Status { get; set; }
+		public string Enrollment { get; set; }
+		public string OldMountingTechnology { get; set; }
+		public string OldEncapsulationType { get; set; }
+		public string ShortDescription { get; set; }
+		public string Category { get; set; }
+		public bool IsUsingInventory { get; set; }
+		public int CurrentAmount { get; set; }
+		public int MinAmount { get; set; }
+		public int MaxAmount { get; set; }
 		public string? Container { get; set; }
 		public string? Location { get; set; }
 		public string? BranchOffice { get; set; }
 		public string? Rack { get; set; }
 		public string? Shelf { get; set; }
-		public decimal? BuyPrice { get; set; }
-		public string? OldUnitType { get; set; }
+		public decimal BuyPrice { get; set; }
+		public string OldUnitType { get; set; }
 		public string? OldManufacturer { get; set; }
 		public string? PartNumber { get; set; }
-		public string? TypeOfStock { get; set; }
-		public bool? IsManualProfit { get; set; }
+		public string TypeOfStock { get; set; }
+		public bool IsManualProfit { get; set; }
 		public decimal? PercentageOfProfit { get; set; }
 		public decimal? PercentageOfDiscount { get; set; }
 		public decimal? SalePriceWithoutDiscount { get; set; }
 		public decimal? SalePriceWithDiscount { get; set; }
 		public decimal? ProfitWithoutDiscount { get; set; }
 		public decimal? ProfitWithDiscount { get; set; }
-		public string? FullDescription { get; set; }
+		public string FullDescription { get; set; }
 		public string? Memo { get; set; }
 		public int Entrys { get; set; }
 		public int Devolutions { get; set; }
@@ -58,41 +58,30 @@ namespace Mux.Model
 		public Product()
 		{
 			Id = 0;
-			DebugCode = "";
-			Status = "";
+			Status = "ACTIVO";
 			Enrollment = "";
 			OldMountingTechnology = "";
 			OldEncapsulationType = "";
-			Category = "";
 			ShortDescription = "";
-			FullDescription = "";
-			IsUsingInventory = true;
+			Category = "";
+			IsUsingInventory = false;
 			CurrentAmount = 0;
 			MinAmount = 0;
 			MaxAmount = 0;
-			Container = "";
-			Location = "";
-			BranchOffice = "";
-			Rack = "";
-			Shelf = "";
 			BuyPrice = 0.0M;
 			OldUnitType = "";
-			TypeOfStock = "";
-			OldManufacturer = "";
-			PartNumber = "";
+			TypeOfStock = "Stock";
 			IsManualProfit = true;
-			PercentageOfProfit = 0.0M;
-			SalePriceWithoutDiscount = 0.0M;
-			ProfitWithoutDiscount = 0.0M;
-			PercentageOfDiscount = 0.0M;
-			SalePriceWithDiscount = 0.0M;
-			ProfitWithDiscount = 0.0M;
+			FullDescription = "";
 			Entrys = 0;
-			Egresses = 0;
 			Devolutions = 0;
+			Egresses = 0;
 			AmountAdjustments = 0;
 			PriceAdjustments = 0;
-			Memo = "";
+			ShoppingCart = new List<ProductToBuy>();
+			ProductRequests = new List<ProductRequest>();
+			ProductChangeLogs = new List<ProductChangelog>();
+			Categories = new List<Category>();
 		}
 	}
 }
