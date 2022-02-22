e5766e4cdc7e5acd7d24f53f32ae4da4ccae5f0c
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 11:02:09 -0600

    Add AddCommentsColumn migration

---
+++ Mux/Migrations/20220222165605_AddCommentsColumn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class AddCommentsColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "ShoppingCart",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "ShoppingCart");
+        }
+    }
+}
