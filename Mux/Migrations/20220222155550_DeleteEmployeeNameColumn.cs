a710a8ffe190e34c78aed59f316611060df51ec2
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 10:04:44 -0600

    Add DeleteEmployeeNameColumn migration

---
+++ Mux/Migrations/20220222155550_DeleteEmployeeNameColumn.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class DeleteEmployeeNameColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployeeName",
+                table: "ShoppingCart");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeName",
+                table: "ShoppingCart",
+                type: "varchar(35)",
+                unicode: false,
+                maxLength: 35,
+                nullable: true);
+        }
+    }
+}
