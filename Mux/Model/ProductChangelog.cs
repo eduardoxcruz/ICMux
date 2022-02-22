7bcd0e655e68dcd0b7020a560239b4977ee6ca28
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 14:08:00 -0600

    Refactorize class

--- Mux/Model/ProductChangelog.cs
+++ Mux/Model/ProductChangelog.cs
@@ -1,40 +1,32 @@
 ﻿using System;
 
+#nullable enable
+
 namespace Mux.Model
 {
 	public class ProductChangelog
 	{
-#pragma warning disable 8632
-		public int? Id { get; set; }
-		public DateTime? Date { get; set; }
-		public string? Type { get; set; }
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public string Type { get; set; }
 		public int? Amount { get; set; }
 		public int? PreviousAmount { get; set; }
 		public int? NewAmount { get; set; }
 		public decimal? PurchasePrice { get; set; }
-		public string? OldProvider { get; set; }
-		public string? ProductFullDescription { get; set; }
-		public string? EmployeeName { get; set; }
-		public Employee Employee { get; set; }
+		public string OldProvider { get; set; }
 		public int? EmployeeId { get; set; }
-		public Product Product { get; set; }
+		public Employee Employee { get; set; }
 		public int? ProductId { get; set; }
+		public Product Product { get; set; }
 		public int? ProviderId { get; set; }
 		public Provider Provider { get; set; }
-#pragma warning restore 8632
 		
 		public ProductChangelog()
 		{
 			Id = 0;
 			Date = DateTime.Now;
 			Type = "";
-			Amount = 0;
-			PreviousAmount = 0;
-			NewAmount = 0;
-			PurchasePrice = 0;
 			OldProvider = "";
-			ProductFullDescription = "";
-			EmployeeName = "";
 		}
 	}
 }
