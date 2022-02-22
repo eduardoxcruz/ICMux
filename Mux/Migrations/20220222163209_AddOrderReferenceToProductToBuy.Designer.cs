f7b737883b511c59dae3b283829274f527e8abfc
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 10:38:41 -0600

    Add AddOrderReferenceToProductToBuy

--- Mux/Migrations/20220222163209_AddOrderReferenceToProductToBuy.Designer.cs
+++ Mux/Migrations/20220222163209_AddOrderReferenceToProductToBuy.Designer.cs
@@ -3,17 +3,19 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mux;
 
 #nullable disable
 
-namespace Inventory.Migrations
+namespace Mux.Migrations
 {
     [DbContext(typeof(ICContext))]
-    partial class ICContextModelSnapshot : ModelSnapshot
+    [Migration("20220222163209_AddOrderReferenceToProductToBuy")]
+    partial class AddOrderReferenceToProductToBuy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Mux/Migrations/20220222163209_AddOrderReferenceToProductToBuy.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mux.Migrations
+{
+    public partial class AddOrderReferenceToProductToBuy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.AddColumn<string>(
+		        name: "OrderReference",
+		        table: "ShoppingCart",
+		        type: "nvarchar(50)",
+		        maxLength: 50,
+		        nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+	        migrationBuilder.DropColumn(
+		        name: "OrderReference", 
+		        table: "ShoppingCart");
+        }
+    }
+}
