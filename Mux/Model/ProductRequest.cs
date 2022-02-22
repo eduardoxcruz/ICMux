0f9affea6a8fd17765061de8ab378bbb1058b0d0
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 14:15:29 -0600

    Refactorize class

--- Mux/Model/ProductRequest.cs
+++ Mux/Model/ProductRequest.cs
@@ -1,28 +1,26 @@
 ﻿using System;
 
+#nullable enable
+
 namespace Mux.Model
 {
 	public class ProductRequest
 	{
-#pragma warning disable 8632
-		public int? Id { get; set; }
-		public DateTime? Date { get; set; }
-		public int? Amount { get; set; }
-		public string? EmployeeName { get; set; }
-		public string? Type { get; set; }
-		public string? Status { get; set; }
+		public int Id { get; set; }
+		public DateTime Date { get; set; }
+		public int Amount { get; set; }
+		public string Type { get; set; }
+		public string Status { get; set; }
 		public Employee Employee { get; set; }
 		public int? EmployeeId { get; set; }
 		public Product Product { get; set; }
 		public int? ProductId { get; set; }
-#pragma warning restore 8632
 		
 		public ProductRequest()
 		{
 			Id = 0;
 			Date = DateTime.Now;
 			Amount = 0;
-			EmployeeName = null;
 			Type = "";
 			Status = "";
 		}
