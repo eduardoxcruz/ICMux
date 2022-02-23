f76c1f9b7e0c5dea0198fcfd7dbab5325ecd9326
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 21:54:39 -0600

    Add Lock property

--- UnitTests/ICDatabaseFixture.cs
+++ UnitTests/ICDatabaseFixture.cs
@@ -3,4 +3,5 @@
 public class IcDatabaseFixture
 {
     private const string ConnectionString = "Server=192.168.0.254;Database=Testing;User Id=sa;Password=Tlacua015;";
+    private static readonly object Lock = new();
 }
