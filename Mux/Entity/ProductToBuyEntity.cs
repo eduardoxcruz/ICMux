bf50bf55d9dc7938637d95b7534162f43e5db9b8
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 18:10:18 -0600

    Refactorize Configure() method

--- Mux/Entity/ProductToBuyEntity.cs
+++ Mux/Entity/ProductToBuyEntity.cs
@@ -12,31 +12,72 @@ namespace Mux.Entity
 
 			builder
 				.Property(productForBuy => productForBuy.Id)
-				.ValueGeneratedNever();
-
-			builder.Property(productForBuy => productForBuy.RequestedAmount);
-
-			builder.Property(productForBuy => productForBuy.RequestDate);
-
-			builder.Property(productToBuy => productToBuy.LocationDate);
-			
-			builder.Property(productToBuy => productToBuy.ArrivalDate);
-
-			builder
-				.Property(productForBuy => productForBuy.OldProvider)
-				.HasMaxLength(35)
-				.IsUnicode(false);
+				.ValueGeneratedOnAdd();
 
 			builder
 				.Property(productForBuy => productForBuy.Status)
-				.HasMaxLength(10)
+				.HasMaxLength(30)
 				.IsUnicode(false);
-
+			
 			builder.Property(productToBuy => productToBuy.OrderReference)
 				.HasMaxLength(50)
 				.IsUnicode();
-			
+
+			builder.Property(productToBuy => productToBuy.LocationDate);
+
+			builder.Property(productToBuy => productToBuy.ArrivalDate);
+
 			builder.Property(productToBuy => productToBuy.Comments)
+				.HasMaxLength(100)
+				.IsUnicode();
+
+			builder.Property(productToBuy => productToBuy.RequestDate);
+
+			builder.Property(productToBuy => productToBuy.InternalReference)
+				.HasMaxLength(50)
+				.IsUnicode();
+
+			builder.Property(productToBuy => productToBuy.ProductDescription)
+				.HasMaxLength(100)
+				.IsUnicode();
+
+			builder.Property(productForBuy => productForBuy.RequestedAmount);
+			
+			builder.Property(productToBuy => productToBuy.MountingTechnology)
+				.HasMaxLength(30)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.EncapsulationType)
+				.HasMaxLength(50)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.Priority)
+				.HasMaxLength(30)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.OriginalProductPirce)
+				.HasPrecision(8, 6)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.IvaIncluded);
+			
+			builder.Property(productToBuy => productToBuy.Currency)
+				.HasMaxLength(10)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.ExtraCost)
+				.HasPrecision(6, 2)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.ProductPriceWithIva)
+				.HasPrecision(8, 6)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.SubTotal)
+				.HasPrecision(8, 6)
+				.IsUnicode();
+			
+			builder.Property(productToBuy => productToBuy.ProductUrl)
 				.HasMaxLength(100)
 				.IsUnicode();
 		}
