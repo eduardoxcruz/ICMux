41a9971ea96ea88742373cb115ae61e61dbdc770
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 20:41:22 -0600

    Add public properties

--- Mux/Model/ProductCategories.cs
+++ Mux/Model/ProductCategories.cs
@@ -4,5 +4,8 @@
 
 internal class ProductCategories
 {
-	
+	public int? ProductId { get; set; }
+	public Product? Product { get; set; }
+	public int? CategoryId { get; set; }
+	public Category Category { get; set; }
 }
