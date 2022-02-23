da369b2988eb7cde21397981b51554c8ccaa3dc8
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 20:43:26 -0600

    Change Category property as nullable

--- Mux/Model/ProductCategories.cs
+++ Mux/Model/ProductCategories.cs
@@ -7,5 +7,5 @@ public class ProductCategories
 	public int? ProductId { get; set; }
 	public Product? Product { get; set; }
 	public int? CategoryId { get; set; }
-	public Category Category { get; set; }
+	public Category? Category { get; set; }
 }
