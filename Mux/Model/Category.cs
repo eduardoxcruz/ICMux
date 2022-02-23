c6884e391ccd5aed1efce18b437b76d8b5ba83a8
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 20:45:12 -0600

    Add ProductCategories property

--- Mux/Model/Category.cs
+++ Mux/Model/Category.cs
@@ -7,6 +7,7 @@ namespace Mux.Model
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public ICollection<Product> Products { get; set; }
+		public List<ProductCategories> ProductCategories { get; set; }
 
 		public Category()
 		{
