ed979b8e61e349cb2e0f67543f983f8d03fb83cc
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 21:54:55 -0600

    Add DatabaseInitialized property

--- UnitTests/ICDatabaseFixture.cs
+++ UnitTests/ICDatabaseFixture.cs
@@ -4,4 +4,5 @@ public class IcDatabaseFixture
 {
     private const string ConnectionString = "Server=192.168.0.254;Database=Testing;User Id=sa;Password=Tlacua015;";
     private static readonly object Lock = new();
+    private static bool DatabaseInitialized;
 }
