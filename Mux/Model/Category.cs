8f1b082e18a9ca8d12c8bae21b92ad6064b4b367
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 20:44:23 -0600

    Replace Products type to ICollection<>

--- Mux/Model/Category.cs
+++ Mux/Model/Category.cs
@@ -6,7 +6,7 @@ namespace Mux.Model
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public List<Product> Products { get; set; }
+		public ICollection<Product> Products { get; set; }
 
 		public Category()
 		{
