13b98dc105c07292dce0a8e29151677cceece27f
José Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2022-02-22 22:04:35 -0600

    Add CreateContext() method

--- UnitTests/ICDatabaseFixture.cs
+++ UnitTests/ICDatabaseFixture.cs
@@ -1,8 +1,20 @@
-﻿namespace UnitTests;
+﻿using Microsoft.EntityFrameworkCore;
+using Mux;
+
+namespace UnitTests;
 
 public class IcDatabaseFixture
 {
     private const string ConnectionString = "Server=192.168.0.254;Database=Testing;User Id=sa;Password=Tlacua015;";
     private static readonly object Lock = new();
     private static bool DatabaseInitialized;
+
+    public ICContext CreateContext()
+    {
+        return new ICContext(new DbContextOptionsBuilder<ICContext>()
+            .UseSqlServer(ConnectionString)
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging()
+            .Options);
+    }
 }
